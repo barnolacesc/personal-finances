be16750812fbb9a81acdf4acdd1673aa382cd8f1
barnolacesc <francescbarnolacalls@gmail.com>
2025-05-03 23:37:55 +0200

    Change port to 5001 to avoid conflicts with AirPlay

--- app.py
+++ app.py
@@ -45,4 +45,4 @@ def handle_expenses():
     return jsonify([expense.to_dict() for expense in expenses])
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True) 
+    app.run(host='0.0.0.0', port=5001, debug=True) 
